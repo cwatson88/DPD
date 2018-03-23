537ca7b258f3e0989dc954c78aa200f37929259a
Chris Watson <cwatson1988@gmail.com>
2018-03-23 19:44:23 +0000

    Final Version of LPCA

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 
 import Badge from "material-ui/Badge";
@@ -6,40 +6,58 @@ import IconButton from "material-ui/IconButton";
 import Comment from "material-ui-icons/Comment"; //Icons are in PascalCase
 import PostComments from "./PostComments";
 
-const Posts = props => {
-  return (
-    <Fragment>
-      {props.posts.map(post => (
-        <div key={post.id} className="post__container">
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-          <p>
-            Posted on: {post.date ? post.date : Date()} by{" "}
-            <b>
-              {
-                // the find will find the correct author by ID but will return an object
-                // React does not like objects as props.
-                // Once found the find feature needs to have .name to return the name prop from the object
-                props.authors.find(
-                  author => author.id === post.userId && author.name
-                ).name
-              }
-            </b>
-          </p>
-          <hr />
-          <IconButton>
-            <Badge badgeContent={post.comments.length} color="primary">
-              <Comment />
-            </Badge>
-          </IconButton>
-          {post.comments.map(comment => (
-            <PostComments key={comment.id} comments={comment} />
-          ))}
-        </div>
-      ))}
-    </Fragment>
-  );
-};
+class Posts extends Component {
+  state = {
+    commentsVisible: false
+  };
+
+   componentDidMount() {
+    this.setState({commentsVisible: false})
+   }
+  toggleComments = this.toggleComments.bind(this);
+
+  toggleComments() {
+    this.setState({ ...this.state, commentsVisible: !this.state.commentsVisible });
+  }
+  render() {
+    const { posts, authors } = this.props;
+    return (
+      <Fragment>
+        {posts.map(post => (
+          <div key={post.id} className="post__container">
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <p>
+              Posted on: {post.date ? post.date : Date()} by{" "}
+              <b>
+                {
+                  // the find will find the correct author by ID but will return an object
+                  // React does not like objects as props.
+                  // Once found the find feature needs to have .name to return the name prop from the object
+                  authors.find(
+                    author => author.id === post.userId && author.name
+                  ).name
+                }
+              </b>
+            </p>
+            <hr />
+            <IconButton onClick={this.toggleComments}>
+              <Badge badgeContent={post.comments.length} color="primary">
+                <Comment />
+              </Badge>
+            </IconButton>
+            {post.comments.map(
+              comment =>
+                this.state.commentsVisible && (
+                  <PostComments key={comment.id} comments={comment} />
+                )
+            )}
+          </div>
+        ))}
+      </Fragment>
+    );
+  }
+}
 
 Posts.propTypes = {
   posts: PropTypes.array
